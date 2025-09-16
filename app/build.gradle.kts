2a7ce799595c5d1157ed6f4b987c27d580688d03
AAK <kamyana786138@gmail.com>
2025-09-16 23:10:27 +0500

    updates

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.aak.al_tabreed"
         minSdk = 23
         targetSdk = 36
-        versionCode = 12
-        versionName = "2.1"
+        versionCode = 13
+        versionName = "2.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
